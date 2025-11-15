a2df79eb827b5d2c637b1b61c02f0bd57bd478e8
VaNiesha Honani <vhonani77@gmail.com>
2025-11-15 09:54:48 -0800

    forgot to add comment - new user assignment

--- User.py
+++ User.py
@@ -1,1 +1,23 @@
-# your User class goes here
+
+class User:
+    """Class User to handle user sign up information"""
+    def __init__(self, name, user_name, email, drivers_license) -> None:
+        self.name = name
+        self.user_name = user_name
+        self.email = email
+        self.drivers_license_num = drivers_license
+
+    def __str__(self):
+        """function to create new user and print out"""
+        return (f"User: {self.name}\n"
+                f"User Name: {self.user_name}\n" 
+                f"Email: {self.email}\n"
+                f"Drivers License: {self.drivers_license_num}\n"
+        )
+
+new_user1 = User("Vee", "vee_test", "v@email.com", "ZZZ11DL")
+new_user2 = User("Doug", "dg001", "d@email.com", "xsssd")
+new_user3 = User("Rex", "trex001", "dino@email.com", "XGL-1")
+print(new_user1)
+print(new_user2)
+print(new_user3)
